81a049ad01b2a744150793b37b6726cce4eecd42
4inches-usbstick <75965179+4inches-usbstick@users.noreply.github.com>
2020-12-31 23:22:17 -0500

    Add files via upload

---
+++ media/upload1process.php
@@ -0,0 +1,31 @@
+<title>engine page</title>
+<?php
+		
+ini_set('upload_max_filesize', '12M');
+ini_set('post_max_size', '12M');
+ini_set('max_input_time', 300);
+ini_set('max_execution_time', 300);
+//echo("Chatbox directory: $_GET[chatnum1]");
+error_reporting(0);
+		
+     
+//echo "<p>" . $_POST['ftu'] . "</p>";
+
+$file_name = $_FILES['ftu']['name'];
+$file_tmp = $_FILES['ftu']['tmp_name'];
+$chatboxdir = $_POST["hidden"];
+$target_dir = ("C:/wamp64/www/textengine/sitechats/media/$chatboxdir/uploaded/$file_name");
+
+move_uploaded_file($file_tmp, $target_dir); 
+//rename($target_dir/$file_name, $target_dir/$file_name/$chatboxdir);
+echo($target_dir . "<br>You can navigate back to chat page");
+
+$URL = $_SERVER['HTTP_REFERER'];
+header("Location: $URL");
+			
+           
+            
+        
+		
+		
+		?>
---
+++ media/uploadform.php
@@ -0,0 +1,42 @@
+
+ 
+<style>
+
+input[type=text] {
+  border: 1px solid LightSlateGrey;
+  border-radius: 4px;
+}
+
+input[type=text]:focus{
+  outline: 2px solid Crimson;     /* oranges! yey */
+}
+
+</style>
+<h2>Upload file page</h2><hr>
+<?php
+echo("
+<title>Upload page $_GET[chatnum1]</title>
+        <form action=\"upload1process.php\" method=\"POST\" enctype=\"multipart/form-data\">
+            <input type=\"file\" name=\"ftu\">
+			<input type=\"hidden\" name=\"hidden\" value=$_GET[chatnum1]>
+            <input type=\"submit\" name=\"submit\" style=\"color:black\">
+        </form>
+		
+		<hr>
+		<!--a href=\"http://71.255.240.10:8080/textengine/sitechats/inchat.php?chatnum=$_GET[chatnum1]&refreshrate=$_GET[rr]&explorer=0\">Back to Chatbox</a--><br>
+		Return to previous page after uploading files
+		<hr>
+
+
+      
+		
+		
+		
+<!--Possible Outcomes:
+1. It appears the same after submitting == Success <p></p>
+2. Error pops up == Failure <p></p>
+3. Brower hangs == Failure <p></p>
+
+");
+
+
