3dad394793c5608096dd96c86dab639a1fbffe08
4inches-usbstick <75965179+4inches-usbstick@users.noreply.github.com>
2021-01-05 18:57:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+http_response_code(403)
+?>
+<head>
+<title>403 Forbidden</title>
+</head>
+
+<body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access this resource.</p>
+<hr>
+<address>Apache/2.4.46 (Win64) PHP/7.3.21 Server at 71.255.240.10 Port 8080</address>
+</body>
+
