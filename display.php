9a5f6c51f7afaa0b09de09e739c8dd4190e951c4
4inches-usbstick <75965179+4inches-usbstick@users.noreply.github.com>
2021-02-08 16:48:17 -0500

    Add files via upload

--- display.php
+++ display.php
@@ -3,5 +3,13 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Cache-Control');
 $chatbox = $_GET["chatbox"];
+
+$iframe = substr_count($_GET["chatbox"], '.hta');
+
+if ($iframe > 0) {
+	header("HTTP/1.0 403 Forbidden");
+	die("403 Forbidden");
+}
+
 echo(file_get_contents($chatbox));
 ?>
