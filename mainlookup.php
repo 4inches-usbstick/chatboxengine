a384542649f4477a1f3042ee38f843c46694f7f2
Charles <infoforchacha@gmail.com>
2021-02-27 21:36:13 -0500

    Changed media upload behavior and created main config parser

---
+++ mainlookup.php
@@ -0,0 +1,25 @@
+<?php
+//get the value of a policy
+function plsk($pid) {
+	$f = file_get_contents("C:/wamp64/www/textengine/sitechats/.htamainpolicy");
+	$fs = explode('::', $f);
+	return $fs[$pid];
+}
+//verify the identity of a UID
+function uidlsk($uid, $skey) {
+	$f = file_get_contents("C:/wamp64/www/textengine/sitechats/.htamainpolicy");
+	$offset0 = strpos($f, '[BEGIN CBAUTH]');
+	$offset1 = strpos($f, '[END CBAUTH]');
+	$fs = substr($f, $offset0, $offset1 - $offset0);
+	$ffs = explode(';', $fs);
+	$user = $ffs[$uid];
+	$userats = explode('::', $user);
+	$ckey = $userats[2];
+	//echo($ckey);
+	if ($ckey == $skey) {
+		return true;
+	} else {
+		return false;
+	}
+}
+?>
--- media/upload1process.php
+++ media/upload1process.php
@@ -1,12 +1,36 @@
 <title>engine page</title>
 <?php
-		
 ini_set('upload_max_filesize', '12M');
-ini_set('post_max_size', '12M');
+ini_set('post_max_size', '14M');
 ini_set('max_input_time', 300);
 ini_set('max_execution_time', 300);
+ini_set('memory_limit', '128M');
+		$phpFileUploadErrors = array(
+    0 => 'OK',
+    1 => 'PAYLOAD TOO BIG (PHP.INI)',
+    2 => 'PAYLOAD TOO BIG (HTML)',
+    3 => 'PARTIAL UPLOAD',
+    4 => 'BLANK PAYLOAD',
+    6 => 'NO TMP DIR',
+    7 => 'WRITING FAILURE',
+    8 => 'EXTENSION CONFLICT',
+);
+
+
 //echo("Chatbox directory: $_GET[chatnum1]");
-error_reporting(0);
+error_reporting(1);
+echo('Bytes: ' . $_FILES['ftu']['size'] . '<br>PHP Status: ' . $_FILES['ftu']['error'] . '::' . $phpFileUploadErrors[$_FILES['ftu']['error']] . '<br>');
+
+if (isset($_FILES['ftu'])) {
+    if ($_FILES['ftu']['size'] > 12582912) {
+				echo('Stop: Illegal payload size detected, halted<br>');
+				die();
+    } else {
+        $b = 'b';
+    }
+}
+
+
 		
      
 //echo "<p>" . $_POST['ftu'] . "</p>";
@@ -18,7 +42,7 @@ $target_dir = ("C:/wamp64/www/textengine/sitechats/media/$chatboxdir/uploaded/$f
 
 move_uploaded_file($file_tmp, $target_dir); 
 //rename($target_dir/$file_name, $target_dir/$file_name/$chatboxdir);
-echo($target_dir . "<br>");
+echo('Path: ' . $target_dir . "<br>");
 
 $URL = $_SERVER['HTTP_REFERER'];
 //header("Location: $URL");
