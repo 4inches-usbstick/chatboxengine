9d22870381f34579bbe60741eca0e270903bbc06
Charles <infoforchacha@gmail.com>
2021-04-11 00:15:52 -0400

    added CLIST cmd

--- terminalprocess.php
+++ terminalprocess.php
@@ -12,7 +12,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Cache-Control");
 
-//error_reporting(plsk(37));
+error_reporting(plsk(37));
 
 $pass = file_get_contents("$rdir/sitechats/.htapassword");
 $useduid = false;
@@ -540,6 +540,37 @@ if ($_GET["cmd"] == "cbroadcast" && $_GET["pass"] == $pass) {
 	}
 	
 }
+
+//
+if ($_GET["cmd"] == "clist") {
+	$chatboxes = glob("*");
+	echo("All Chatboxes:<br><br>");
+	foreach($chatboxes as $i) {
+		if (is_dir($i)) {
+		//echo("$i: DIR-SKIP<br>");
+		goto skipexec1;
+		} 
+		//if is dir
+		
+		if (substr_count($i, ".") > 0 && substr_count($i, ".html") == 0 && substr_count($i, ".cbedata") == 0) {
+		goto skipexec1;
+		}
+		//if there is a dot and it"s not HTML
+		if (substr_count(wr_db(), $i) != 0) {
+		$v = ' : protected';
+		} else {
+			$v = ' : open';
+		}
+		
+		if (substr_count($i, ".html") > 0 || substr_count($i, ".") == 0) {
+		echo("$i $v<br>\n"); 
+		}
+		//if there are no dots or it"s HTML
+		skipexec1:
+		
+	}
+	
+}
 //cload
 if ($_GET["cmd"] == "cload" && $_GET["pass"] == $pass) {
 copy("$rdir/sitechats/copies/$_GET[params]", "$rdir/sitechats/$_GET[params]");
@@ -568,6 +599,7 @@ xedit: brings up the remote message editing terminal, no parameters<br><br>
 
 *cbroadcast: broadcast message with contents (parameter) to all legacy and HTML chatboxes. Use DRYRUN to show which files will be written to without actually writing.<br>
 *^_copen: open a Chatbox. parameters slot syntax: FILENAME.FILE-EXT --MEDIAOPTION<br>
+clist: list all Chatboxes and whether or not they are protected<br>
 *loadexe: sideloads an extension. requires an active RDC connection that is listening to .htaremotedesktop and the sideloader extension. this command can be disabled with PID 15.<br>
 help: brings up this help message, no parameters<br><br>
 
